8fc807ad7ce7f8777df45c548a56117192c148b4
DeoTimeTheGithubUser <89555032+DeoTimeTheGithubUser@users.noreply.github.com>
2022-06-21 03:12:23 -0500

    Initial Commit v2

--- src/main/java/org/dtrade/DTrade.java
+++ src/main/java/org/dtrade/DTrade.java
@@ -3,4 +3,10 @@ package org.dtrade;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class DTrade extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("Hello!");
+    }
+
 }
